444fff9f4b4c20c94691f3ef4a864f50aadd6103
Julia Resende Fernandes <juliaresende270@gmail.com>
2023-07-28 12:15:09 -0300

    update

---
+++ teste_ss.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+Python_Dir = os.path.dirname(os.path.abspath(__file__))
+Data = os.path.join(Python_Dir, 'Data.json')
+
+with open(Data, 'r') as arquivo_json:
+    dados_lidos = json.load(arquivo_json)
+    
+#Adicionar contato   
+#Tel = input("telefone ")
+#name = input("nome ")
+#dados_lidos[Tel] = name
+
+print(dados_lidos)
+
+#Deletar
+Del = input("deletar nome ")
+del dados_lidos[Del in dados_lidos.values()]
+print(dados_lidos)
